c4680c5d553bd0c0cd59a584d7ff92b26d13dcbe
Rafaelg2t <rafael.rodrigues200518@gmail.com>
2026-04-04 19:20:57 +0100

    Add base version of the Earth satellite simulator

--- main.py
+++ main.py
@@ -22,3 +22,38 @@ ax.add_patch(earth)
 #Orbit
 
 orbit = plt.Circle((0,0), ray_orbit, fill=True)
+ax.add_patch(orbit)
+
+#Satelite
+
+satelite, = ax.plot([], [], 'o')
+
+
+#Initialisation
+
+def init():
+    satelite.set_data([], [])
+    return satelite,
+
+#Animation Atualization
+
+def update(frame):
+    theta = np.radians(frame)
+    x = ray_orbit * np.cos(theta)
+    y = ray_orbit * np.sin(theta)
+    satelite.set_data([x], [y])
+    return satelite,
+
+#Animation
+
+anim = FuncAnimation(
+    fig,
+    update
+    frames=np.arange(0,360,2),
+    init_func=init,
+    interval=50,
+    blit=True
+)
+
+
+plt.show()
