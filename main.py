6e379aae6c1cea1281524ba94d1d7f31b2f7db15
Rafaelg2t <rafael.rodrigues200518@gmail.com>
2026-04-05 22:50:40 +0100

    Convert simulator from 2D to initial 3D VPython version

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import math
 
 #-----------Simulations Values-------------
 
-ray_earth      = 6371   # km
-ray_orbit      = 42164  # km
-angle_step     = 0.02   # degreed per frame
-scale          = 1/1000 # scale of visual to 3D
+ray_earth        = 6371   # km
+ray_orbit        = 42164  # km
+angle_step       = 0.02   # degreed per frame
+scale            = 1/1000 # scale of visual to 3D
+ray_earth_visual = ray_earth * scale #earth size in scale
+ray_orbit_visual = ray_orbit * scale #orbit size in scale
+
+
 #-----------Create figure and axes-------------
 
 scene = canvas(
     title = 'Earth Satelites Simulator 3D ',
-    width =4500,
-    height =4500,
+    width  = 4500,
+    height = 4500,
     background = color.black
 )
 
@@ -21,24 +25,37 @@ scene = canvas(
 
 #-----------Earth-------------
 
-earth = plt.Circle((0,0), ray_earth, fill=True)
-ax.add_patch(earth)
+earth = sphere(
+    pos=vector(0,0,0),
+    radius= ray_earth_visual,
+    texture=textures.earth
 
+)
 
 
 
 
 #-----------Orbit-------------
 
-orbit = plt.Circle((0,0), ray_orbit, fill=False, linestyle='--', linewidth=1.5)
-ax.add_patch(orbit)
+orbit_path = curve(color=color.white, radius=0.02)
+
+for angle in range(0, 361, 2):
+    theta = math.radians(angle)
+    x = ray_orbit_visual * math.cos(theta)
+    z = ray_orbit_visual * math.sin(theta) 
+    orbit_path.append(vector(x,0,z))
 
 
 
 
 #------------Satelite------------
 
-satelite, = ax.plot([], [], 'o', markersize=8)
+satelite = sphere(
+    pos= vector(ray_orbit_visual,0,0),
+    radius = 0.18,
+    color=color.red,
+    make_trail=False
+)
 
 
 
@@ -52,27 +69,17 @@ def init():
 
 
 
-#------------Animation Atualization------------
+#------------Animation Cicle 'loop'------------
 
-def update(frame):
-    theta = np.radians(frame)
-    x = ray_orbit * np.cos(theta)
-    y = ray_orbit * np.sin(theta)
-    satelite.set_data([x], [y])
-    return satelite,
+theta = 0
 
+while True:
+    rate(100)
 
+    x = ray_orbit_visual * math.cos(theta)
+    z = ray_orbit_visual * math.sin(theta) 
 
+    satelite.pos = vector(x,0,z)
+    theta += angle_step
 
-#-----------Animation-------------
-
-anim = FuncAnimation(
-    fig,
-    update,
-    frames=np.arange(0, 360, angle_step),
-    init_func=init,
-    interval=frame_interval,
-    blit=True
-)
-
-plt.show()
+    
